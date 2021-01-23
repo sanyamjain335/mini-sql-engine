5b70c4dfd1af6d5c71b60f3318570802328b9309
Sanyam Jain <sanyam.jain@students.iiit.ac.in>
2021-01-23 10:36:22 +0530

    Finalizing output

--- 2020201006.py
+++ 2020201006.py
@@ -7,7 +7,7 @@ from sqlparse.sql import Where, Comparison, Parenthesis, Identifier
 
 # Read CSV file wise
 
-path = 'files'
+path = '.'
 extension = 'csv'
 os.chdir(path)
 files = glob.glob('*.{}'.format(extension))
@@ -81,6 +81,8 @@ query = sys.argv[1]
 # Get column names to fetch
 
 query = query.split(' ')
+while "" in query:
+    query.remove("")
 query = [x.lower() for x in query]
 start_word = "select"
 end_word = "from"
@@ -88,7 +90,6 @@ start_index = query.index(start_word)
 end_index = query.index(end_word)
 
 col_names = " ".join(query[start_index+1:end_index])
-
 if query[query.index('from') + 1][-1] == ';':
     tables = query[query.index('from') + 1][:-1].split(',')
 else:
@@ -102,7 +103,8 @@ else:
 res = []
 aggregate_functions = ['max','min','avg','count','sum']
 keywords = ['where','group','order']
-operators  = ['=','>','<','>=','<=']
+operators  = ['=','>=','<=','>','<']
+has_aggregate_func = False
 if len(tables) == 1:                        # Single table
     #print(col_names)
     try:
@@ -124,41 +126,161 @@ if len(tables) == 1:                        # Single table
         if condition[-1] == ';':
             condition = condition[:-1]
 
-        # Without AND, OR
-        operator = [o for o in operators if o in condition][0]
-        cond_table = condition.split(operator)[0]
-        val = int(condition.split(operator)[1])
+        if 'and' in condition:
+            conditions = condition.split('and')
+            for condition in conditions:
+                operator = [o for o in operators if o in condition][0]
+                cond_table = condition.split(operator)[0]
+                val = int(condition.split(operator)[1])
 
-        cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() == cond_table][0]
-        cols_to_remove = []
-        for x in res:
-            if operator == '=':
-                if int(x[cols]) != val:
-                    cols_to_remove.append(x)
+                try:
+                    cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() == cond_table][0]
+                    cols_to_remove = []
+                    for x in res:
+                        if operator == '=':
+                            if int(x[cols]) != val:
+                                cols_to_remove.append(x)
 
-            elif operator == '<':
-                if int(x[cols]) >= val:
-                    cols_to_remove.append(x)
+                        elif operator == '<':
+                            if int(x[cols]) >= val:
+                                cols_to_remove.append(x)
 
-            elif operator == '>':
-                if int(x[cols]) <= val:
-                    cols_to_remove.append(x)
+                        elif operator == '>':
+                            if int(x[cols]) <= val:
+                                cols_to_remove.append(x)
 
-            elif operator == '>=':
-                if int(x[cols]) < val:
-                    cols_to_remove.append(x)
+                        elif operator == '>=':
+                            if int(x[cols]) < val:
+                                cols_to_remove.append(x)
 
-            elif operator == '<=':
-                if int(x[cols]) > val:
-                    cols_to_remove.append(x)                
+                        elif operator == '<=':
+                            if int(x[cols]) > val:
+                                cols_to_remove.append(x)                
+                    
+                    for x in cols_to_remove: 
+                        res.remove(x)
+                except:
+                    print("Col doesn't exist")
+                    sys.exit()
+
+        elif 'or' in condition:
+            conditions = condition.split('or')
+            
+            condition = conditions[0]
+
+            operator = [o for o in operators if o in condition][0]
+            cond_table = condition.split(operator)[0]
+            val = int(condition.split(operator)[1])
+
+            try:
+                cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() == cond_table][0]
+                cols_to_remove = []
+                for x in res:
+                    if operator == '=':
+                        if int(x[cols]) != val:
+                            cols_to_remove.append(x)
+
+                    elif operator == '<':
+                        if int(x[cols]) >= val:
+                            cols_to_remove.append(x)
+
+                    elif operator == '>':
+                        if int(x[cols]) <= val:
+                            cols_to_remove.append(x)
+
+                    elif operator == '>=':
+                        if int(x[cols]) < val:
+                            cols_to_remove.append(x)
+
+                    elif operator == '<=':
+                        if int(x[cols]) > val:
+                            cols_to_remove.append(x)
+            except:
+                print("Col doesn't exist")
+                sys.exit()         
+            
+            condition = conditions[1]
+
+            operator = [o for o in operators if o in condition][0]
+            cond_table = condition.split(operator)[0]
+            val = int(condition.split(operator)[1])
+
+            try:
+                cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() == cond_table][0]
+                temp_cols_to_remove = []
+                for x in cols_to_remove:
+                    if operator == '=':
+                        if int(x[cols]) == val:
+                            temp_cols_to_remove.append(x)
+
+                    elif operator == '<':
+                        if int(x[cols]) < val:
+                            temp_cols_to_remove.append(x)
+
+                    elif operator == '>':
+                        if int(x[cols]) > val:
+                            temp_cols_to_remove.append(x)
+
+                    elif operator == '>=':
+                        if int(x[cols]) >= val:
+                            temp_cols_to_remove.append(x)
+
+                    elif operator == '<=':
+                        if int(x[cols]) <= val:
+                            temp_cols_to_remove.append(x)
+            except:
+                print("Col doesn't exist")
+                sys.exit()                    
+            
+            for x in temp_cols_to_remove:
+                cols_to_remove.remove(x)        
+            
+            for x in cols_to_remove: 
+                res.remove(x)
+                
         
-        for x in cols_to_remove: 
-            res.remove(x)
+        # Without AND, OR
+        else:
+            operator = [o for o in operators if o in condition][0]
+            cond_table = condition.split(operator)[0]
+            val = int(condition.split(operator)[1])
+
+            try:
+                cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() == cond_table][0]
+                cols_to_remove = []
+                for x in res:
+                    if operator == '=':
+                        if int(x[cols]) != val:
+                            cols_to_remove.append(x)
+
+                    elif operator == '<':
+                        if int(x[cols]) >= val:
+                            cols_to_remove.append(x)
+
+                    elif operator == '>':
+                        if int(x[cols]) <= val:
+                            cols_to_remove.append(x)
+
+                    elif operator == '>=':
+                        if int(x[cols]) < val:
+                            cols_to_remove.append(x)
+
+                    elif operator == '<=':
+                        if int(x[cols]) > val:
+                            cols_to_remove.append(x)                
+                
+                for x in cols_to_remove: 
+                    res.remove(x)
+            except:
+                print("Col doesn't exist")
+                sys.exit()
 
 
     if 'distinct' in col_names:
         col_names = col_names.replace('distinct ','')
         col_names = col_names.split(',')
+        col_names = [x.strip(' ') for x in col_names]
+
         cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() in col_names]
         if len(cols) == 0:
             print("Column not found")
@@ -177,10 +299,12 @@ if len(tables) == 1:                        # Single table
     elif any(x in col_names for x in aggregate_functions):              # Contains aggregate functions
         # TODO Handle * in this case
 
+        has_aggregate_func = True
         if 'max' in col_names:
             col_names = col_names.replace('max(','')
             col_names = col_names.replace(')','')
             col_names = col_names.split(',')
+            col_names = [x.strip(' ') for x in col_names]
             cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() in col_names]
             
             if len(cols) == 0:
@@ -200,6 +324,7 @@ if len(tables) == 1:                        # Single table
             col_names = col_names.replace('min(','')
             col_names = col_names.replace(')','')
             col_names = col_names.split(',')
+            col_names = [x.strip(' ') for x in col_names]
             cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() in col_names]
 
             if len(cols) == 0:
@@ -219,6 +344,7 @@ if len(tables) == 1:                        # Single table
             col_names = col_names.replace('sum(','')
             col_names = col_names.replace(')','')
             col_names = col_names.split(',')
+            col_names = [x.strip(' ') for x in col_names]
             cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() in col_names]
 
             if len(cols) == 0:
@@ -237,24 +363,31 @@ if len(tables) == 1:                        # Single table
             col_names = col_names.replace('count(','')
             col_names = col_names.replace(')','')
             col_names = col_names.split(',')
-            cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() in col_names]
+            col_names = [x.strip(' ') for x in col_names]
+            if col_names[0] == '*':
+                total_rows = len(res)
+                res = []
+                res.append([total_rows])
+            else:
+                cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() in col_names]
 
-            if len(cols) == 0:
-                print("Column not found")
-                sys.exit()
-            
-            count = 0
-            for row in res:
-                temp_row = [row[i] for i in cols]
-                count += 1
+                if len(cols) == 0:
+                    print("Column not found")
+                    sys.exit()
+                
+                count = 0
+                for row in res:
+                    temp_row = [row[i] for i in cols]
+                    count += 1
 
-            res = []
-            res.append([count])
+                res = []
+                res.append([count])
 
         elif 'avg' in col_names:
             col_names = col_names.replace('avg(','')
             col_names = col_names.replace(')','')
             col_names = col_names.split(',')
+            col_names = [x.strip(' ') for x in col_names]
             cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() in col_names]
 
             if len(cols) == 0:
@@ -274,6 +407,7 @@ if len(tables) == 1:                        # Single table
 
     elif col_names != '*':
         col_names = col_names.split(',')
+        col_names = [x.strip(' ') for x in col_names]
         cols = [i-1 for i in range(len(table_columns[index_of_table])) if table_columns[index_of_table][i].lower() in col_names]
         if len(cols) == 0:
             print("Column not found")
@@ -285,10 +419,44 @@ if len(tables) == 1:                        # Single table
     if 'order' in query:
         order_by_col = query[query.index('order')+2]
         col_index = table_columns[index_of_table].index(order_by_col.upper())-1
-        asc_desc = query[query.index('order')+3]
-        if asc_desc == 'desc':
-            res = sorted(res,key=lambda x: (x[col_index]), reverse=True)
-        else:
-            res = sorted(res,key=lambda x: (x[col_index]))
+        try:
+            asc_desc = query[query.index('order')+3]
 
-print(res)
+            if asc_desc == 'desc':
+                res = sorted(res,key=lambda x: (x[col_index]), reverse=True)
+            else:
+                res = sorted(res,key=lambda x: (x[col_index]))
+        except:
+            print("Invalid order")
+            sys.exit()
+
+# Display output
+
+if has_aggregate_func:
+    print("<{}.{}>".format(table_columns[index_of_table][0],col_names[0].upper()))
+    print(res[0][0])
+else:
+    if col_names == '*':
+        print("<", end=" ")    
+        for i in range(1, len(table_columns[index_of_table])):
+            if i == len(table_columns[index_of_table])-1:
+                print("{}.{}".format(table_columns[index_of_table][0], table_columns[index_of_table][i].upper()), end=" ")
+            else:
+                print("{}.{}".format(table_columns[index_of_table][0], table_columns[index_of_table][i].upper()), end=", ")
+        print(">")
+
+    else:
+        temp_cols = [s.lower() for s in table_columns[index_of_table]]
+        col_names = sorted(col_names,key=temp_cols.index)
+
+        print("<", end=" ")
+        for x in col_names:
+            if col_names.index(x) == len(col_names)-1:
+                print("{}.{}".format(table_columns[index_of_table][0], x.upper()), end=" ")
+            else:
+                print("{}.{}".format(table_columns[index_of_table][0], x.upper()), end=", ")
+        print(">")
+    for x in res:
+        for y in x:
+            print(y, end=" ")
+        print("")
